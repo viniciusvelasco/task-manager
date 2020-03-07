18a0cb99ee4b3ee4a1835c38a3b39c971e685fab
Vinícius Velasco <vinicius.velasco@live.com>
2020-03-07 19:38:26 -0400

    Criação de módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TaskService } from './tasks/shared/task.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
